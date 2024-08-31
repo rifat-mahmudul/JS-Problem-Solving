5c27fe5cd3d1426958b2bd8af8a7c5ac7b54d9bf
Mahmudul Hasan Rifat <mahmudulhasanrifat201@gmail.com>
2024-08-31 16:06:00 +0600

    problem 4 solved

---
+++ problem-4.js
@@ -0,0 +1,18 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice, cokePrice;
+
+burgerPrice = 500;
+
+if(burgerPrice === 500){
+    console.log("Coke is free")
+}
+
+else{
+    console.log("coke price 30tk")
+}
